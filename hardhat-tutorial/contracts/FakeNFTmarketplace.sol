108677710d134491069405ad0393851931d60a3b
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-10-13 10:32:21 +0530

    first commit

---
+++ hardhat-tutorial/contracts/FakeNFTmarketplace.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+contract FakeNFTmarketplace {
+    mapping(uint256 => address) public tokens;
+    uint256 nftprice = 0.1 ether;
+
+    function purchase(uint256 _tokenID) external payable {
+        require(msg.value == nftprice, "This NFT cost 0.1 ether");
+        tokens[_tokenID] = msg.sender;
+    }
+    function getPrice() external view returns(uint256){
+        return nftprice;
+    }
+
+
+    
+     function available (uint256 _tokenID) external view returns (bool) {
+         if(tokens[_tokenID]==address(0)){
+             return true;
+         }
+         return false;
+    }
+
+
+}
+
+
+
